0ae4e46983fa9f217a751e004540fcf7b5f39ea6
Malik Cameron <malikcameron95@gmail.com>
2024-11-14 21:46:27 -0800

    Firt start in implementing a nueral network

--- network.py
+++ network.py
@@ -8,11 +8,23 @@ import os
 chocolate_dataset_train = datasets.ImageFolder('data')
 
 dataset_tuple = chocolate_dataset_train.imgs
-print(chocolate_dataset_train.class_to_idx)
+chocolate_dataset_train.class_to_idx
+dataset_tuple
+
+clss_vals = chocolate_dataset_train.class_to_idx.keys()
+idx_vals = list(chocolate_dataset_train.class_to_idx.values())
 
 img_idx = {}
 
+
+#Building pandas dataframe
+img_idx.fromkeys(clss_vals)
+
 for img_path in dataset_tuple:
     _,idx = img_path
+    position = idx_vals.index(idx)
+    print(position)
+
+### ultimately dataframe should look like clss, img as cols..each data in row formatted 
 
 
