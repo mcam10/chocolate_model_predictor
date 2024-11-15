e60774766e4ba26cd71b12e3dfdd73646bde0f4b
Malik Cameron <malikcameron95@gmail.com>
2024-11-14 21:20:50 -0800

    Firt start in implementing a nueral network

--- network.py
+++ network.py
@@ -1,9 +1,18 @@
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
+import torchvision.transforms as transforms
+import pandas as pd
+import os
 
-chocolate_dataset = datasets.ImageFolder('data')
+chocolate_dataset_train = datasets.ImageFolder('data')
 
-print(chocolate_dataset)
+dataset_tuple = chocolate_dataset_train.imgs
+print(chocolate_dataset_train.class_to_idx)
+
+img_idx = {}
+
+for img_path in dataset_tuple:
+    _,idx = img_path
 
 
