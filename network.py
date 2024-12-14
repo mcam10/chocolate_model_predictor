04fb14518bd103fcfba1d877a5be326d48c46921
Malik Cameron <malikcameron95@gmail.com>
2024-12-13 17:28:42 -0800

    First pass at a network next is to visualize

--- network.py
+++ network.py
@@ -1,8 +1,24 @@
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
+import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.transforms as transforms
 import pandas as pd
 import os
+import matplotlib as plt
 
-chocolate_dataset_train = datasets.ImageFolder('data')
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 sqaure convs
+        self.conv1 = nn.Conv2d(1,6,5)
+    def forward(self, input):
+        ## Convolution layer C!: 1 input image channel, 6 output channels, 
+        c1 = F.relu(self.conv1(input))
+
+net = Net()
+print(net)
+
+params = list(net.parameters())
+print(params[0].size())
